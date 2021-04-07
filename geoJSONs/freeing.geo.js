726ea1259fc5f589bfa01aa894c7dd01a6241b3b
Matt <matthewkschultz@gmail.com>
2021-04-06 23:31:29 -0400

    v0.1 working

---
+++ geoJSONs/freeing.geo.js
@@ -0,0 +1,286 @@
+var freeingSpaces = [{
+		type: 'Feature',
+		properties: {
+			admin: 'The Zapatistas (EZLN)',
+			freed: true
+		},
+		geometry: {
+			type: 'Polygon',
+			coordinates: [[
+					[-92.75420216367333, 17.342785394334136],
+					[-92.67870628812908, 17.382816421406705],
+					[-92.66612364220504, 17.45284964458905],
+					[-92.5948219819688, 17.492856556614704],
+					[-92.51932610642454, 17.57184433224591],
+					[-92.51932610540672, 17.570844709868844],
+					[-92.35260604691314, 17.619820087221253],
+					[-92.34841183160512, 17.69875238523164],
+					[-92.27291595606086, 17.688762869701936],
+					[-92.19532297286261, 17.77365601785433],
+					[-92.06635085054192, 17.702748036077534],
+					[-91.92689319155045, 17.8465320886881],
+					[-91.79267830169397, 17.824572572885007],
+					[-91.8293776856391, 17.735708758409718],
+					[-91.9008372154416, 17.56832120072696],
+					[-90.65828655076494, 16.600463141649346],
+					[-90.95840601054879, 16.089080428793874],
+					[-91.7325459255052, 16.07290980602765],
+					[-91.92888576273917, 15.735724514747243],
+					[-92.99670982014288, 16.19937594416359],
+					[-92.55811371581582, 16.728531874198808],
+					[-93.0627350401491,17.057949522272487]
+			]]
+		}
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'The Foundry',
+			freed: true,
+			website: 'https://bravosfoundry.com'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [-7.641724521729832,43.601181745652475]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'La Borda',
+			freed: true,
+			website: 'http://www.laborda.coop/en/',
+			description: 'Collectively owned cooperative housing project'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [2.134867347748791,41.369601042553995]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Willy Fred',
+			freed: true,
+			website: 'https://www.willy-fred.org/',
+			description: 'Collectively owned self-organized housing project'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [14.290021074883375,48.30618879428193]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Autonome Wohnfabrik',
+			freed: true,
+			website: 'https://autonome-wohnfabrik.at/',
+			description: 'Collectively owned self-organized housing project'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.93741036550858,52.36441569406879]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Soweto',
+			freed: true,
+			website: 'https://soweto.nl/en/about/projects',
+			description: 'Social housing cooperative'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.93745328043885,52.36440259035811]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Regenbogenfabrik',
+			freed: true,
+			website: 'https://www.regenbogenfabrik.de/',
+			description: 'Former squat turned project space / hostel (solidary economy, direct democracy)'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [13.426970900007491,52.49634286609234]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Sostre Civid',
+			freed: true,
+			website: 'https://sostrecivic.coop/',
+			description: 'Tenant ownership / cooperative housing organization'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [2.174009899997204,41.39256441889115]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Ecodorp Boekel',
+			freed: true,
+			website: 'https://www.ecodorpboekel.nl/',
+			description: 'Eco-village in the Netherlands; member of VrijCoop'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [5.680674131342632,51.59576087299225]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Bajesdorp',
+			freed: true,
+			website: 'https://bajesdorp.nl/',
+			description: 'Cooperative housing / artist studio project in a former prison area; member of VrijCoop'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.9273452039872385,52.336133681163346]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'De Groene Gemeenschap',
+			freed: true,
+			website: 'https://degroenegemeenschap.org/',
+			description: 'Cooperative housing project; member of VrijCoop, Soweto and Mietshäuser Syndikat'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.9880281158816535,52.36454472498336]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Meer Dan Wonen',
+			freed: true,
+			website: 'https://meerdanwonen.org',
+			description: `(Maybe doesn't exist anymore?)`
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.9880281158816535,52.36454472498336]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Rijkshemelvaartdienst',
+			freed: true,
+			website: 'http://www.rijkshemelvaart.com/',
+			description: 'Former squat, now space for artists to work and live; member of VrijCoop'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.806939869054294,52.33474261687354]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Pension Almonde',
+			freed: true,
+			website: 'https://www.pension-almonde.nl',
+			description: 'Temporary home for urban nomads in an area planned to be demolished; member of Stad in de Maak'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.481239206550786,51.92892777616899]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Banierstraat 62',
+			freed: true,
+			website: 'https://www.stadindemaak.nl/banierstraat-62/',
+			description: 'Several artist studios in a former marijuana plantation; member of Stad in de Maak'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.472396993561466,51.930871285416316]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Bloklandstraat 190',
+			freed: true,
+			website: 'https://www.stadindemaak.nl/bloklandstraat-190/',
+			description: 'Housing / workspace; member of Stad in de Maak'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.478445076690659,51.937102208317825]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Pieter de Raadtstraat 35 & 37',
+			freed: true,
+			website: 'https://www.stadindemaak.nl/pieter-de-raadtstraat-35-37/',
+			description: 'Housing / workspace; member of Stad in de Maak'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.4689701640104,51.928947559868874]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Zwaanshals 288B',
+			freed: true,
+			website: 'https://www.stadindemaak.nl/zwaanshals-288-b/',
+			description: 'Housing / workspace; member of Stad in de Maak'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.486100363355468,51.933358617107096]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Nieuwland',
+			freed: true,
+			website: 'https://nieuwland.cc/',
+			description: 'Housing / project space'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.929990179281179,52.362402089807176]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'Nieuwland',
+			freed: true,
+			website: 'https://nieuwland.cc/',
+			description: 'Housing / project space'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [4.929990179281179,52.362402089807176]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'La Porcheritz',
+			freed: true,
+			website: 'http://clip.ouvaton.org/les-projets/',
+			description: 'Housing freed from the market; member of le Clip'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [2.406255346955154,48.86564242506829]
+		},
+	}, {
+		type: 'Feature',
+		properties: {
+			admin: 'La Déviation',
+			freed: true,
+			website: 'http://www.ladeviation.org/',
+			description: 'Space for artistic research; member of le Clip'
+		},
+		geometry: {
+			type: 'Point',
+			coordinates: [5.306144439693702,43.36858777928976]
+		},
+	}
+];

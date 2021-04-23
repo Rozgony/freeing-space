90d32506a6a4af3831c94b08e5aeec05d26369ef
Matt <matthewkschultz@gmail.com>
2021-04-23 14:57:11 -0400

    more design options

--- scripts/map.js
+++ scripts/map.js
@@ -1,20 +1,20 @@
+const freeingSpacesJSON = freeingSpacesFromCSV.map(convertToGeoJSON);
+const basicOSM = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const watercolorOSM = 'http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
+const topoOSM = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+
 const map = L.map('map').setView([0, 0], 2);
 
-const freeingSpacesJSON = freeingSpacesFromCSV.map(convertToGeoJSON);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer(topoOSM, {
 	maxZoom: 18,
+	continuousWorld: true,
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
 		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	id: 'mapbox/light-v9',
 	tileSize: 512,
 	zoomOffset: -1,
-	boundary: 'aboriginal_lands'
 }).addTo(map);
 
 function onEachFeature(feature, layer) {
-	console.log({feature})
 	const props = feature.properties;
 	let text = props.url ? `<a href=${props.url} target="_blank">${props.admin}</a>` : props.admin;
 	if (props.description) {
@@ -30,9 +30,9 @@ L.geoJSON(freeingSpacesJSON, {
 	onEachFeature: onEachFeature,
 	pointToLayer: function (feature, latlng) {
 		return L.circleMarker(latlng, {
-			radius: 4,
-			fillColor: "#ff7800",
-			color: "#000",
+			radius: 6,
+			fillColor: 'red',
+			color: '#000',
 			weight: 1,
 			opacity: 1,
 			fillOpacity: 0.8
